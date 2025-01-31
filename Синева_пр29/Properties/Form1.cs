c78028e5457e089c694027804e214bdad9eb7280
SSL <ivahnolera5@gmail.com>
2025-01-31 11:52:50 +0300

    Добавлено окно с вопросом

--- Синева_пр29/Properties/Form1.cs
+++ Синева_пр29/Properties/Form1.cs
@@ -16,5 +16,14 @@ namespace Синева_пр29.Properties
         {
             InitializeComponent();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var result = MessageBox.Show("Вы действительно хотите выйти?", "Выход", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes) {
+                Application.Exit();
+            }
+
+        }
     }
 }
